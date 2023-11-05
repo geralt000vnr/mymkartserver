9b2d76242abe6fc70c6fb367f17372568360ac8f
geralt000vnr <86870066+geralt000vnr@users.noreply.github.com>
2023-11-05 17:04:12 +0530

    initial comit

---
+++ server.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const app = express();
+const port = process.env.PORT || 4000;
+const { MongoClient } = require("mongodb");
+const uri = "mongodb://localhost:27017/mymkart"; // Replace with your MongoDB connection string
+const client = new MongoClient(uri, { useNewUrlParser: true });
+
+app.get("/", (req, res) => {
+  res.send("Hello, Express!");
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
+
+async function connectToMongoDB() {
+  try {
+    await client.connect();
+    console.log("Connected to MongoDB");
+  } catch (error) {
+    console.error("Error connecting to MongoDB:", error);
+  }
+}
+
+connectToMongoDB();
